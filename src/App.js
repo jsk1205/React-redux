509ffd928030a4c36266847cfdd3fa1e2ea9e170
xiaodi <2657032439@qq.com>
2020-04-30 00:09:25 +0800

    总结

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Count from './containers/Count'
+import Count from './containers/Count' //不在入口js文件做处理-->会报错
 
 export default class App extends Component {
   render() {

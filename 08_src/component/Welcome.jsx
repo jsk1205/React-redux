0d59d00f5c687c4082e8d0a6a20815f6126376be
xiaodi <2657032439@qq.com>
2020-05-04 22:00:26 +0800

    更新

--- 08_src/component/Welcome.jsx
+++ 08_src/component/Welcome.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
-//引入store
-import store from '../redux/store'
-
-export default class Count extends Component {
-  
+//引入connect方法(重点)
+import { connect } from 'react-redux' 
+import {increment} from "../redux/actions/count";
+ class Welcome extends Component {
+  add=()=>{
+		//函数体
+		this.props.increment()
+	}
   render() {
-		const {count,personCount} = this.props
+		// const {count,personCount} = this.props
 		// console.log(this.props.count)
 		//console.log(store.getState())
 		//console.log(store.getState().number)
 		//console.log(store.getState().persons.length)
     return ( 
 			<div>
-        <h1>所念皆星河</h1>
-				<h2>当前求和为:{store.getState().number},组件总人数为:{store.getState().persons.length}</h2>
+        <h1>所念皆星河,求和为:{this.props.he},总人数为:{this.props.ren}</h1>
+				{/* <h2>当前求和为:{store.getState().number},组件总人数为:{store.getState().persons.length}</h2> */}
 		    {/* <h2>当前求和为:{count},组件总人数为:{personCount}</h2> */}
+        <button onClick={this.add}>change</button>			
 			</div>
     )
   }
 }
+export default connect(
+	state=({
+		he:state.number,
+		ren:state.persons.length
+	}),
+	{increment}
+)(Welcome)
--- 08_src/redux/reducers/index.js
+++ 08_src/redux/reducers/index.js
@@ -2,7 +2,7 @@
 import count from './count'
 import personReducer from './person'
 import {combineReducers} from 'redux'
-import welcome from "./welcome";
+
 export  default combineReducers({//总的
   number:count,
   persons:personReducer
